a9dd355c3bfadfe4f76aa58b6a638599e6fd6b01
KrakenTim <KrakenTim@freenet.de>
2022-04-06 16:50:25 +0200

    Added Bullets And made the turret working

--- SpiritJam/Assets/Scripts/Turret Script/Turret_Shooting.cs
+++ SpiritJam/Assets/Scripts/Turret Script/Turret_Shooting.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Turret_Shooting : MonoBehaviour
 {
+    public GameObject Bullet;
+    public Transform BulletSpawner;
+    public float cooldown;
+    private float firerate;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        firerate = cooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        firerate -= Time.deltaTime;
+        if (firerate<=0)
+        {
+            Instantiate(Bullet, BulletSpawner.transform.position, Quaternion.identity);
+            firerate = cooldown;
+        }
     }
 }
