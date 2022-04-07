2a3a251265f62a994027e4334590c7438dd76221
KrakenTim <KrakenTim@freenet.de>
2022-04-07 14:19:37 +0200

    Turret has range now

--- SpiritJam/Assets/Scripts/Turret Script/Turret_Shooting.cs
+++ SpiritJam/Assets/Scripts/Turret Script/Turret_Shooting.cs
@@ -1,26 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Turret_Shooting : MonoBehaviour
 {
     public GameObject Bullet;
-    public Transform BulletSpawner;
+    public GameObject BulletSpawner;
     public float cooldown;
+    public float range; 
+    [SerializeField]
+    private bool PlayerInRange;
     private float firerate;
+    private GameObject Player;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        Player = GameObject.FindGameObjectWithTag("Player");
         firerate = cooldown;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Math.Sqrt(((BulletSpawner.transform.position.x-Player.transform.position.x)*(BulletSpawner.transform.position.x - Player.transform.position.x))+ ((BulletSpawner.transform.position.y - Player.transform.position.y) * (BulletSpawner.transform.position.y - Player.transform.position.y))+ ((BulletSpawner.transform.position.z - Player.transform.position.z) * (BulletSpawner.transform.position.z - Player.transform.position.z))) <= range)
+        {
+            PlayerInRange = true;
+        }
+        else
+        {
+            PlayerInRange = false;
+        }
+
+
         firerate -= Time.deltaTime;
-        if (firerate<=0)
+        if (firerate<=0&&PlayerInRange)
         {
             Instantiate(Bullet, BulletSpawner.transform.position, Quaternion.identity);
             firerate = cooldown;
