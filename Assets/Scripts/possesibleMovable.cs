cadae98ca77458cbf884349b1efc682d507fd210
Kyss007 <keanucarlo@hotmail.com>
2022-04-08 12:14:00 +0200

    added possible nonmoveable

--- Assets/Scripts/possesibleMovable.cs
+++ Assets/Scripts/possesibleMovable.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class possesibleBasic: MonoBehaviour
+public class possesibleMovable : MonoBehaviour
 {
     private PlayerInputActions inputActions;
     private Cinemachine.CinemachineFreeLook cinemachineFreeLook;
+    private Rigidbody RB;
     private GameObject Player;
 
+    bool grounded = false;
     bool possed = false;
 
     private void Awake() {
         inputActions = new PlayerInputActions();
         inputActions.player.Enable();
 
-        cinemachineFreeLook = FindObjectOfType<Cinemachine.CinemachineFreeLook>();        
+        cinemachineFreeLook = FindObjectOfType<Cinemachine.CinemachineFreeLook>();
+
+        RB = GetComponent<Rigidbody>();        
     }
 
     void Update()
@@ -36,12 +40,36 @@ public class possesibleBasic: MonoBehaviour
             inputActions.player.Enable();
             cinemachineFreeLook.LookAt = this.gameObject.transform;
             cinemachineFreeLook.Follow = this.gameObject.transform;
+
+            Vector3 moveDir = calculateDesiredMoveDirection() * 3;
+
+            if (grounded){
+                if(moveDir != Vector3.zero){
+                    moveDir += Vector3.up * 2;
+                }
+
+            } else {
+                moveDir = moveDir / 3;
+            }
+
+            RB.AddForceAtPosition(moveDir, new Vector3(this.transform.position.x, this.transform.position.y + 0.25f, this.transform.position.z), ForceMode.Impulse);
         }else{
             inputActions.player.Disable();
         }
         
     }
 
+    private void OnCollisionStay(Collision other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("possibleObjectGround")){
+            grounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("possibleObjectGround")){
+            grounded = false;
+        }
+    }
 
     private void OnEnable() {
         inputActions.player.posses.performed += dePosses;    
@@ -64,5 +92,24 @@ public class possesibleBasic: MonoBehaviour
             
             possed = false;
         }
+    }
+
+
+
+    private Vector3 calculateDesiredMoveDirection(){
+        float moveVertical = inputActions.player.movement.ReadValue<Vector2>().y;
+        float moveHorizontal = inputActions.player.movement.ReadValue<Vector2>().x;
+
+        Vector3 camForward = Vector3.forward; //camera.transform.forward;
+        Vector3 camRight = Vector3.right; //camera.transform.right;
+
+        camForward.y = 0f;
+        camRight.y = 0f;
+        camForward.Normalize();
+        camRight.Normalize();
+
+        Vector3 desiredMoveDirection = (camForward * moveVertical) + (camRight * moveHorizontal);
+
+        return desiredMoveDirection;
     }
 }
